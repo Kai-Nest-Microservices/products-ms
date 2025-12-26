45d4acb7765ee88f545e1523ec8e89258e058035
Kai <kailinasc2@gmail.com>
2025-12-26 16:27:26 -0300

    fix: update PrismaService to use envs for database URL and adjust Dockerfile for Prisma setup

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
 
+import { envs } from 'src/config';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaBetterSqlite3({ url: envs.databaseURL });
     super({ adapter });
   }
 }
