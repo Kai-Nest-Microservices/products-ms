6a77c6d55bc26affc444e55833200860cbd30535
Kai <kailinasc2@gmail.com>
2025-12-31 17:51:14 -0300

    fix: format imports in PrismaService for consistency

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+
 import { PrismaClient } from '@prisma/client';
 
 import { envs } from 'src/config';
