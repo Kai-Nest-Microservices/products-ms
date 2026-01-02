d74c82a562b62d21303c740c3f6f6d30e548eeb8
Kai <kailinasc2@gmail.com>
2026-01-02 14:39:24 -0300

    debugging bettersql missing env

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -8,10 +8,13 @@ import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { envs } from 'src/config';
 import { PrismaClient } from 'src/generated/prisma/client';
 
+const databaseUrl = envs.databaseURL?.replace('file:', '') || './dev.db';
+
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: envs.databaseURL });
+    const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
     super({ adapter });
   }
 }
